7de96de25384c1f648f550a9ae69cf76e0aa8220
Sandro Felicioni <sandro.felicioni@gmail.com>
2014-10-21 12:48:30 +0200

    Added TiniIR export project

---
+++ TiniIRExport/src/main/ScallaWrapper.java
@@ -0,0 +1,10 @@
+package main;
+
+import ch.ethz.dal.tinyir.searchEngine.SearchEngine;
+
+public class ScallaWrapper {
+
+	public static void main(String[] args) {
+		SearchEngine.main(args);
+	}
+}
